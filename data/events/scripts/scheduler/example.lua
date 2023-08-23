daaa9cc1ba2d4a6034972cbbe8cf2156c0f6d516
Ruan Santos <ruan.santos@infracommerce.com.br>
2023-08-23 16:34:27 -0300

    Fix to show vip xp bonus on exp rate at skills

--- data/events/scripts/scheduler/example.lua
+++ data/events/scripts/scheduler/example.lua
@@ -15,11 +15,3 @@ function Example_One.onStartup()
 end
 
 Example_One:register()
-
-local Example_Two = GlobalEvent("Example two")
-function Example_Two.onThink(interval)
-	return true
-end
-
-Example_Two:interval(10000) -- 10 seconds interval
-Example_Two:register()
